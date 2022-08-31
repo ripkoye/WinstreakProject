92588842f63a6fb609fc31d3f0575d6c6696a31b
Ryan Dang <ripkoyework@gmail.com>
2022-08-31 12:42:21 -0400

    added a semiworkable GUI

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # WinstreakProject
+currently need to work on:
+    revise gui.py and edit out unecessary lines
+    use functions from main.py to simplify gui.py
+    make a button to have the winstreak working or not working (true or false)
---
+++ gui.py
@@ -0,0 +1,32 @@
+#import all of tkinter and also updated widgets
+from tkinter import *
+from tkinter import ttk
+import main
+
+#sets up main application window
+root = Tk()
+root.title("Winstreak Counter GUI")
+
+#creating a content frame (frame widget, hold content of user interface)
+#we do this because it'll allow us to control the background color and allow for more themed widgets
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=10, row=10, sticky=(N, W, E, S))
+root.columnconfigure(10, weight=1)
+root.rowconfigure(10, weight=1)
+
+f = open('/Users/ripkoye/Desktop/WinstreakProject/winstreak.txt', 'r')
+#store the information into a variable
+winstreak = StringVar(mainframe)
+winstreak.set(f.read())
+#creates a winstreaklabel
+winstreaklabel = ttk.Label(mainframe,width=100)
+winstreaklabel.grid()
+winstreaklabel['textvariable'] = winstreak
+#assigns the text to the variable named winstreak
+f.close()
+
+button = ttk.Button(mainframe)
+
+root.mainloop()
+
+
