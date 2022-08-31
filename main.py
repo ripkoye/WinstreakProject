ea9cb3fb1ddb015ee7489a0cfb32947219176b64
Ryan Dang <ripkoyework@gmail.com>
2022-08-31 09:09:17 -0400

    winstreak should be working

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import requests
 import json
 
-player = 'koearn'
+uuid = '9621788a942d48bbbfb6f05484e4dca2'
 headers = {
+    'uuid': '9621788a942d48bbbfb6f05484e4dca2',
     'API-Key': '5434a1b3-6c62-4db5-8449-b6ab2109cb89',
-    'uuid': '9621788a942d48bbbfb6f05484e4dca2'
 }
 
-response = requests.get('https://api.hypixel.net/player', headers=headers)
+response = requests.get('https://api.hypixel.net/player?uuid='+uuid,headers=headers)
 
 data = response.json()
 
 success = data['success']
-cause = data['cause']
 
-#wins = data['GameTypes']['Bedwars']['player']['stats']['winstreak']
+wins = data['player']['stats']['Bedwars']['winstreak']
 
 print(success)
-print(cause)
 print(response)
-print(headers)
 
-#f = open("/Users/ripkoye/Desktop/WinstreakProject/winstreak.txt", "w")
-#f.write("Winstreak: "+str(wins))
-#f.close()
+f = open("/Users/ripkoye/Desktop/WinstreakProject/winstreak.txt", "w")
+f.write("Winstreak: "+str(wins))
+f.close()
