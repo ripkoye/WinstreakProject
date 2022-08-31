0c2de92f5c7b728cb6bd7d1137d4eb47c5fe05e9
Ryan Dang <ripkoyework@gmail.com>
2022-08-31 10:40:16 -0400

    added comments and showed data rate

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ import json
 
 uuid = '9621788a942d48bbbfb6f05484e4dca2'
 headers = {
-    'uuid': '9621788a942d48bbbfb6f05484e4dca2',
     'API-Key': '5434a1b3-6c62-4db5-8449-b6ab2109cb89',
 }
 
+#requests to the api (winstreak data and rate information)
 response = requests.get('https://api.hypixel.net/player?uuid='+uuid,headers=headers)
+responserate = requests.get('https://api.hypixel.net/key', headers=headers)
 
+#making it readable
 data = response.json()
+datarate = responserate.json()
 
+#pull the information out from json
 success = data['success']
-
 wins = data['player']['stats']['Bedwars']['winstreak']
 
+#function to write the information into the txt file for streamlabs to run
+def write():
+    f = open("/Users/ripkoye/Desktop/WinstreakProject/winstreak.txt", "w")
+    f.write("Winstreak: "+str(wins))
+    f.close()
+
+#calling and showing response
+write()
 print(success)
 print(response)
+print(datarate)
 
-f = open("/Users/ripkoye/Desktop/WinstreakProject/winstreak.txt", "w")
-f.write("Winstreak: "+str(wins))
-f.close()
