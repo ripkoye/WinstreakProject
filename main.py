0ca84ec2f270c64c368599d4db9f36d1494f96eb
Ryan Dang <ripkoyework@gmail.com>
2022-08-30 22:32:13 -0400

    trying to get uuid sent to hypixel servers

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import requests
-player = ''
+import json
+
+player = 'koearn'
 headers = {
-    'AUTHORIZATIONS': 'something',
+    'API-Key': '5434a1b3-6c62-4db5-8449-b6ab2109cb89',
+    'uuid': '9621788a942d48bbbfb6f05484e4dca2'
 }
-response = requests.get('https://api.hypixel.net/'+player, headers=headers)
 
-#wins = response['player']['stats']['wins']
+response = requests.get('https://api.hypixel.net/player', headers=headers)
 
-wins = 3
+data = response.json()
 
-f = open("/Users/ripkoye/Desktop/WinstreakProject/winstreak.txt", "w")
-f.write("Winstreak: "+str(wins))
-f.close()
+success = data['success']
+cause = data['cause']
+
+#wins = data['GameTypes']['Bedwars']['player']['stats']['winstreak']
+
+print(success)
+print(cause)
+print(response)
+print(headers)
+
+#f = open("/Users/ripkoye/Desktop/WinstreakProject/winstreak.txt", "w")
+#f.write("Winstreak: "+str(wins))
+#f.close()
